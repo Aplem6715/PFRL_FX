7896c48856f3b5469887a8114861b36b9d919841
Aplem6715 <Aplem6715@gmail.com>
2020-10-21 11:13:31 +0900

    報酬計算を行う関数の追加

--- fx_calc.py
+++ fx_calc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 LEVERAGE = 25.0
 SPREAD = 0.003
@@ -13,3 +14,20 @@ def calc_margin(position_unit: float, now_price: float):
 # 損益を計算
 def calc_pl(open_price, now_price, units):
     return (now_price - open_price) * LEVERAGE * units - SPREAD * LEVERAGE * np.sign(units)
+
+
+def calculate_volatility(self, partial_price_arr, window_size):
+    alpha = 2 / float(window_size + 1)
+    ema_arr = []
+    emvar_arr = []
+    delta = 0
+    ema_arr.append(partial_price_arr[0])
+    emvar_arr.append(delta)
+    for idx in range(1, window_size):
+        delta = partial_price_arr[idx] - ema_arr[idx - 1]
+        ema_arr.append(ema_arr[idx - 1] + alpha * delta)
+        emvar_arr.append(
+            (1 - alpha) * (emvar_arr[idx - 1] + alpha * delta * delta))
+
+    emsd = math.sqrt(emvar_arr[-1])
+    return emsd
--- fx_env_evo.py
+++ fx_env_evo.py
@@ -5,6 +5,8 @@ import numpy as np
 import datetime as dt
 from typing import List
 
+import fx_calc
+
 STAY = 0
 BUY = 1
 SELL = 2
@@ -17,6 +19,16 @@ PIPS_TO_PRICE = 0.01
 
 RISK_THRESH = 0.02
 LOSS_CUT_PIPS = 20
+
+VOLATILITY_TGT = 5.0
+
+
+def action2int(action):
+    if action == BUY:
+        return 1
+    if action == SELL:
+        return -1
+    return 0
 
 
 def pips2price(pips):
@@ -62,6 +74,7 @@ class Broker():
         self.now_time = dt.datetime()
         # 現ステップでのClose値
         self.now_price = 0
+        self.prev_price = 0
 
         # 必要証拠金割合を設定
         self.leverage = leverage
@@ -80,6 +93,20 @@ class Broker():
         # ステップイテレータ
         self.iter = 0
         self.df = df
+
+        self.volatility_arr = []
+        self.setup_volatility_arr(self.df.Close.values.tolist())
+
+    def setup_volatility_arr(self, rate_arr, window_size):
+        local_window_size = window_size
+        for idx in range(len(rate_arr)):
+            if idx + 1 < local_window_size:
+                self.volatility_arr.append(0)
+            else:
+                s = (idx + 1) - local_window_size
+                tmp_arr = rate_arr[s:idx + 1]
+                self.volatility_arr.append(
+                    self.calculate_volatility(tmp_arr, local_window_size))
 
     @property
     def has_long(self):
@@ -104,11 +131,17 @@ class Broker():
     def margin(self):
         return self.position_size * MARGIN_RATIO
 
+    def get_volatility(self, delta):
+        return self.volatility_arr[self.iter + delta]
+
     # 内部状態を更新する（各ステップの最初に必ず呼び出す)
+    # return: is_last
     def update(self):
+        self.prev_price = self.now_price
         self.now_price = self.df.Close[self.iter]
         self.now_time = self.df.Datetime[self.iter]
         self.iter += 1
+        return self.iter >= len(self.df)
 
     # ロスカットが必要かどうか
     def need_loss_cut(self):
@@ -159,7 +192,8 @@ class FxEnv(gym.Env):
         self.broker = Broker(MARGIN_RATIO, INIT_BALANCE)
 
     def step(self, action):
-        self.broker.update()
+        done = self.broker.update()
+
         # need_loss_cut = self.broker.need_loss_cut()
         if action == STAY:
             pass
@@ -169,6 +203,9 @@ class FxEnv(gym.Env):
         elif action == SELL and not self.broker.has_short:
             self.broker.close()
             self.broker.sell()
+        self.action_hist.append(action)
+
+        return self.observe(), self.calc_rewerd(), done, {}
 
     def render(self):
         if mode == self.TEST_MODE:
@@ -178,6 +215,26 @@ class FxEnv(gym.Env):
 
     def observe(self):
         pass
+
+    # 利益計算（https://qiita.com/ryo_grid/items/1552d70eb2a8c15f6fd2 参照）
+    def calc_rewerd(self):
+        # 取引量（１でいいらしい
+        mu = 1
+        # 取引コスト
+        bp = 0.0015
+        # アクション履歴(idx=-1は今回(t)のアクション, (t-1)のアクションはidx=-2)
+        A1 = self.action_hist[-2]
+        A2 = self.action_hist[-3]
+        # ボラティリティ
+        sigma1 = self.broker.get_volatility(-1)
+        sigma2 = self.broker.get_volatility(-2)
+        # 定数でいい？計算式がわからん
+        sigma_tgt = VOLATILITY_TGT
+        # 価格履歴
+        p1 = self.broker.prev_price
+        rt = self.broker.now_price - self.broker.prev_price
+
+        return mu*(A1*(sigma_tgt/sigma1) - bp*p1*abs((sigma_tgt/sigma1)*A1 - (sigma_tgt/sigma2)*A2))
 
     def close(self):
         if mode == self.TEST_MODE:
